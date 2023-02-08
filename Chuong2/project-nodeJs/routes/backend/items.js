818c1bce17a8d031857a30670b3be3a73e2ab03c
dncuong <dinhcuongnd2001@gmail.com>
2023-02-08 08:45:48 +0700

    Search Engine

--- Chuong2/project-nodeJs/routes/backend/items.js
+++ Chuong2/project-nodeJs/routes/backend/items.js
@@ -7,26 +7,38 @@ router.get("/list", function (req, res, next) {
   res.render("pages/items/list", { title: "List Items" });
 });
 
-router.get("(/status/:status)?", function (req, res, next) {
+router.get("(/:status)?", function (req, res, next) {
   // ItemsModel la mot doi tuong tham chieu den Items Collection trong db
   const ItemsModel = require("./../../schemas/items");
   const helper = require("./../../helper/utils");
   const paramHelper = require("./../../helper/param");
   let statusFilterCurrent = paramHelper.getParams(req.params, "status", "all");
-  console.log(statusFilterCurrent);
+  let searchCurrent = paramHelper.getParams(req.query, "search", "");
   const statusFilter = helper.createStatusFilter(
     ItemsModel,
     statusFilterCurrent
   );
   // ordering: { $gt: 0, $lt: 100 }
   const conditionFilter =
-    statusFilterCurrent == "all" ? {} : { status: statusFilterCurrent };
+    statusFilterCurrent == "all"
+      ? searchCurrent == ""
+        ? {}
+        : { name: { $regex: searchCurrent, $options: "i" } }
+      : searchCurrent == ""
+      ? { status: statusFilterCurrent }
+      : {
+          status: statusFilterCurrent,
+          name: { $regex: searchCurrent, $options: "i" },
+        };
+
+  // console.log("condition:", conditionFilter);
   ItemsModel.find(conditionFilter, (err, result) => {
     if (err) return console.error("Error in router Items ", err);
     res.render("pages/items/index", {
       title: "this is the index items",
       items: result,
       statusFilter: statusFilter,
+      searchCurrent,
     });
   });
   // ItemsModel.find({}, "name").then((result) => {
