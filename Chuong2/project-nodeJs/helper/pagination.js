0bf2de4194b056c571b6e33c419a560f04f853f6
dncuong <dinhcuongnd2001@gmail.com>
2023-02-11 21:09:39 +0700

    Panigation

--- Chuong2/project-nodeJs/helper/pagination.js
+++ Chuong2/project-nodeJs/helper/pagination.js
@@ -1,13 +1,15 @@
 const ItemMode = require("./../schemas/items");
-const numberElementPerPage = 2;
+const numberElementPerPage = 1;
 const totalElement = 1;
 // const totalElement = ItemMode.countDocuments().then((res) => res);
 // const totalPages = Math.ceil(totalElement / numberElementPerPage);
 const currentPage = 1;
+const pageRange = 5;
 
 module.exports = {
   numberElementPerPage,
   totalElement,
   // totalPages,
   currentPage,
+  pageRange,
 };
